72697ea33a8099993f251fc68654d25b19dcbb5f
zolov <i.zolov@yandex.ru>
2024-09-18 20:35:52 +0300

    Добавлена хоткей для фулскрина для wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.font = wezterm.font_with_fallback({
 { family = "Hacker Nerd Font", scale = 1.3 },
 })
 -- config.font_size = 17
+config.macos_window_background_blur = 30
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
@@ -20,5 +21,26 @@ config.scrollback_lines = 3000
 config.default_prog = { "/opt/homebrew/bin/tmux" }
 
 config.enable_tab_bar = false
+config.mouse_bindings = {
+	  -- Ctrl-click will open the link under the mouse cursor
+	  {
+	    event = { Up = { streak = 1, button = 'Left' } },
+	    mods = 'CTRL',
+	    action = wezterm.action.OpenLinkAtMouseCursor,
+	  },
+	}
+
+config.keys = {
+		{
+			key = 'f',
+			mods = 'CTRL',
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = '\'',
+			mods = 'CTRL',
+			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+		},
+	}
 
 return config
