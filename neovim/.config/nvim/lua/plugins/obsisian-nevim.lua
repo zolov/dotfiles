5e0e1577f26af5796a9b5a182cd288a9c154085d
Igor Zolov <ivzolov@mts.ru>
2024-09-23 13:09:30 +0300

    Obsidian-nvim reformat
    - Добавлена директория с рабочим волтом

--- neovim/.config/nvim/lua/plugins/obsisian-nevim.lua
+++ neovim/.config/nvim/lua/plugins/obsisian-nevim.lua
@@ -1,35 +1,39 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
 
-    -- see below for full list of optional dependencies 👇
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/Documents/Zettelkasten",
-      }
+        -- see below for full list of optional dependencies 👇
     },
-  },
+    opts = {
+        workspaces = {
+            {
+                name = "personal",
+                path = "~/Documents/Zettelkasten",
+            },
+            {
+                name = "work",
+                path = "~/Documents/work-vault",
+            }
+        },
+    },
     -- ui = { enable = false },
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
     completion = {
-    -- Set to false to disable completion.
-    nvim_cmp = true,
-    -- Trigger completion at 2 chars.
-    min_chars = 2,
-  },
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+    },
 }
