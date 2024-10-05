8d9dede6f6d4b2ebd4f07c3cad7ca27a05c7e87d
zolov <i.zolov@yandex.ru>
2024-10-05 23:14:27 +0300

    Добавлены новые плагены и настройки

--- neovim/.config/nvim/lua/zolov/config/autocommands.lua
+++ neovim/.config/nvim/lua/zolov/config/autocommands.lua
@@ -191,3 +191,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
