0812363ef55c5a1253f5ef53081ac71cafcf491d
zolov <i.zolov@yandex.ru>
2024-10-20 18:36:38 +0300

    Colorcolumn = 120

--- neovim/.config/nvim/lua/zolov/plugins/smartcolumn.lua
+++ neovim/.config/nvim/lua/zolov/plugins/smartcolumn.lua
@@ -2,6 +2,7 @@ return {
     "m4xshen/smartcolumn.nvim",
     event = "BufAdd",
     opts = {
+        colorcolumn = "120",
         disabled_filetypes = { "help", "text", "markdown", "lazy", "mason" },
     },
 }
