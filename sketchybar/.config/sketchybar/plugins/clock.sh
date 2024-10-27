03a89e0968ba0930a6f34896cfb665291606aee6
zolov <i.zolov@yandex.ru>
2024-10-27 13:44:48 +0300

    Изменена конфигурация sketchybar [WIP]
    - Используется тема catppuccin для согласованности с системой

--- sketchybar/.config/sketchybar/plugins/clock.sh
+++ sketchybar/.config/sketchybar/plugins/clock.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# The $NAME variable is passed from sketchybar and holds the name of
-# the item invoking this script:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
-
+LABEL=$(date '+%H:%M:%S')
+sketchybar --set "$NAME" label="$LABEL"
